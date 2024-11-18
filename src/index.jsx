5a6fa923ce6eae9dc1026a98a7a4f363df2a0d64
Harsh Singh <sky_walker@Harshs-MacBook-Air.local>
2024-11-18 19:49:57 +0530

    first_add

--- src/index.jsx
+++ src/index.jsx
@@ -4,17 +4,42 @@ import './index.css';
 
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(React.createElement("div", null,
-  React.createElement("header", null,
-  React.createElement("img", {
-  src: "https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/Kalvium-Logo.png",
-  alt: ""
-}),React.createElement("h3", null, "Kalvium Education")),React.createElement("div", null,
-React.createElement("p", null, `It is a long established fact that a reader will
+
+class App extends React.Component {
+  render(){
+    return(
+      <div>
+      <img src="https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/Kalvium-Logo.png" alt="" />
+      <h3>Kalvium Education</h3>
+      <p>It is a long established fact that a reader will
  be distracted by the readablecontent of a page when looking at its layout. The point of using Lorem 
  Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, 
  content here', making it look like readable English. Many desktop publishing packages and web page editors 
  now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites 
  still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on 
- purpose (injected humour and the like).`))));
+ purpose (injected humour and the like).</p>
+      
+      </div>
+    );
+  }
+}
+const root = ReactDOM.createRoot(document.getElementById('root')); // Updated rendering for React 18+
+root.render(<App />);
+
+export default App;
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(React.createElement("div", null,
+//   React.createElement("header", null,
+//   React.createElement("img", {
+//   src: "https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/Kalvium-Logo.png",
+//   alt: ""
+// }),React.createElement("h3", null, "Kalvium Education")),React.createElement("div", null,
+// React.createElement("p", null, `It is a long established fact that a reader will
+//  be distracted by the readablecontent of a page when looking at its layout. The point of using Lorem 
+//  Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, 
+//  content here', making it look like readable English. Many desktop publishing packages and web page editors 
+//  now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites 
+//  still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on 
+//  purpose (injected humour and the like).`))));
